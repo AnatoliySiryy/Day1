7b7783142937b1ba564ab23fd0bad821b2ca49c4
Anatoliy <90030259+AnatoliySiryy@users.noreply.github.com>
2021-10-21 15:57:13 +0300

    Add files via upload

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,11 +2,11 @@
 <div>
     <table>
                 <tr>
-                    <th>Photo</th>
-                    <th>Name</th>
-                    <th>Group</th>
-                    <th>Mark</th>
-                    <th>PR is Done</th>
+                    <th>Фото</th>
+                    <th>Имя</th>
+                    <th>Группа</th>
+                    <th>Оценка</th>
+                    <th>Практическая</th>
                 </tr>
                 <tr v-for="stud in students" v-bind:key="stud._id">
                     <td><img v-bind:src="stud.photo" width="50px"></td>
@@ -14,19 +14,36 @@
                     <td>{{stud.group}}</td>
                     <td>{{stud.mark}}</td>
                     <td><input type="checkbox" v-bind:checked="stud.isDonePr"></td>
+                    <td><a href="#" v-on:click="deleteStudent(stud._id)">Удалить</a></td>
+                    <td><button v-on:click="getData(stud._id,stud.name,stud.group,stud.mark,stud.isDonePr)"><img src="components/pencil.png" width="20px"></button></td>
                 </tr>
             </table>
 
-            <br>Enter amount: <input type="number" value="100" v-model="start_value">
-            <br>Convert from 
+            <br>Введите фамилию <input v-model="search">
+            <p><h3>Добавить нового студента</h3>
+            <br>Введите имя: <input v-model="name">
+            <br>Введите группу: <input v-model="group">
+            <br>Оценка : <input type="number" v-model="mark">
+            <br>Введите сдана ли Практичная : <input type="checkbox" v-model="isDonePr">
+            <br><button v-on:click="addStudent">Добавить студента</button>
+
+             <p><h3>Обновить студента </h3>
+            <br>Введите имя: <input v-model="name1">
+            <br>Введите группу:: <input v-model="group1">
+            <br>Оценка : <input type="number" v-model="mark1">
+            <br>Введите сдана ли Практическая : <input type="checkbox" v-model="isDonePr1">
+            <br><button v-on:click="updateStudent()">Обновить студента</button>
+            <br>
+            <br>Введите сумму: <input type="number" value="100" v-model="start_value">
+            <br>Конвертировать из 
             <br><input type="radio" value="USD" name="value"  v-model="start_ccy"><label for="USD">USD</label>
             <br><input type="radio" value="EUR" name="value" v-model="start_ccy"><label for="EUR">EUR</label>
             <br><input type="radio" value="RUR" name="value" v-model="start_ccy"><label for="RUR">RUR</label>
-            <br>to
+            <br>в
             <br><input type="radio" value="USD" name="Convert" v-model="end_ccy"><label for="USD">USD</label>
             <br><input type="radio" value="EUR" name="Convert" v-model="end_ccy"><label for="EUR">EUR</label>
             <br><input type="radio" value="RUR" name="Convert" v-model="end_ccy"><label for="RUR">RUR</label>
-            <button v-on:click="convert">Convert</button>
+            <button v-on:click="convert">Конвертировать</button>
             <br>{{result}}
 </div>
 
@@ -40,6 +57,13 @@ import VueAxios from 'vue-axios'
 export default {
     data: function() {
            return {
+            name:"",
+            group:"",
+            isDonePr:false,
+            name1:"",
+            group1:"",
+            isDonePr1:false,
+            studId:"",
             students: [],
             currency:[],
             search:"",
@@ -56,30 +80,64 @@ export default {
             start_value:0,
             end_value:0,
             result:"",
-        }},  
-   methods:
-    {
-        deletePeople:function (id) {
-            this. people =  this. people.filter(elem => elem.id!=id)
-          }
-        
-    },
-    mounted: function(){
+            reload:"",
+            mark:0,
+            mark1:0,
+        }}, 
+         mounted: function(){
         axios.get("http://46.101.212.195:3000/students").then((response)=>{
             console.log(response.data);
             this.students = response.data;
-        })
+           })
         axios.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5").then((response)=>{
             console.log(response.data);
             this.currency = response.data;
-        })
-    },
-    methods:{
-        deleteRow:function(id){
-             this.students =  this.students.filter(stud => stud.id!=id)
+          })
+                  },
+         methods:{
+        
+        addStudent:function(){
+            Vue.axios.post("http://46.101.212.195:3000/students", {
+                name: this.name,
+                group: this.group,
+                mark: this.mark,
+                isDonePr: this.isDonePr,
+            })
+            .then((response) => {
+                console.log(response.data)
+            })
+            axios.get("http://46.101.212.195:3000/students").then((response)=>{
+                this.students = response.data;
+            })
+        },
+        deleteStudent:function(id){
+            Vue.axios.delete("http://46.101.212.195:3000/students/"+id, {
+            })
+            axios.get("http://46.101.212.195:3000/students").then((response)=>{
+                this.students = response.data;
+            })
+        },
+        getData: function(id,name,group,mark,isDone){
+            this.studId = id;
+            this.name1 = name;
+            this.group1 = group;
+            this.mark1 = mark;
+            this.isDonePr1 = isDone;
+        },
+        updateStudent:function(){
+            Vue.axios.put("http://46.101.212.195:3000/students/"+this.studId, {
+                name: this.name1,
+                group: this.group1,
+                mark: this.mark1,
+                isDonePr: this.isDonePr1,
+            })
+            axios.get("http://46.101.212.195:3000/students").then((response)=>{
+                this.students = response.data;
+            })
         },
         convert:function(){
-            for(let i=0; i<this.currency.length; i++){
+            for(let i=0; i<this.currency.length; i++)
+            {
                 if (this.currency[i].ccy==this.start_ccy)
                       this.sell=this.currency[i].sale;
                 if (this.currency[i].ccy==this.end_ccy)
@@ -89,7 +147,7 @@ export default {
             this.result = this.start_value + " " + this.start_ccy + " = " + this.end_value + " " + this.end_ccy;
             
         }
-     }
+        }
 }
 </script>
 <style scoped>
